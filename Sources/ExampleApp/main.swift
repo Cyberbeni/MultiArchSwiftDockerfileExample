edcf56ab0bc690dcfa0bcee6b45189e77f86df62
Benedek Kozma <cyberbeni@gmail.com>
2024-10-09 18:18:55 +0200

    Only call signal on macOS

--- Sources/ExampleApp/main.swift
+++ Sources/ExampleApp/main.swift
@@ -13,7 +13,10 @@ let signalHandlers = [
 	SIGINT, // ctrl+C in interactive mode
 	SIGTERM, // docker container stop container_name
 ].map { signalName in
-	signal(signalName, SIG_IGN)
+	// https://github.com/swift-server/swift-service-lifecycle/blob/24c800fb494fbee6e42bc156dc94232dc08971af/Sources/UnixSignals/UnixSignalsSequence.swift#L80-L85
+	#if canImport(Darwin)
+		signal(signalName, SIG_IGN)
+	#endif
 	let signalSource = DispatchSource.makeSignalSource(signal: signalName, queue: .main)
 	signalSource.setEventHandler {
 		print("Got signal: \(signalName)")
