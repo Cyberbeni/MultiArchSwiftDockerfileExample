c1b51a9a86b3f65c8352e6e2f5f1c55c343141b6
Benedek Kozma <cyberbeni@gmail.com>
2024-10-17 12:30:40 +0200

    also lint when formatting

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,6 +7,8 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null
 if which swift > /dev/null 2>&1; then
 	swift build --product swiftformat
 	./.build/debug/swiftformat --cache ./.build/swiftformat-cache.json .
+	# https://github.com/nicklockwood/SwiftFormat/issues/1904
+	./.build/debug/swiftformat --cache ./.build/swiftformat-cache.json --lint --lenient .
 elif which docker > /dev/null 2>&1; then
 	docker run --rm \
 		--volume .:/workspace \
