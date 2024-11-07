5228025ad0760bba4167a9066aa99d0d5d8706ba
Benedek Kozma <cyberbeni@gmail.com>
2024-11-07 18:30:12 +0100

    Only rebuild swiftformat when needed

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,10 +5,26 @@ set -eo pipefail
 pushd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null
 
 if which swift > /dev/null 2>&1; then
-	swift build --product swiftformat
-	./.build/debug/swiftformat --cache ./.build/swiftformat-cache.json .
+	SWIFTFORMAT="./.build/debug/swiftformat"
+	CACHE="./.build/swiftformat-cache.json"
+
+	# Replicate `make` behaviour in docker, `swift build` can easily take 5+ seconds even when no operations are needed.
+	NEEDS_REBUILD=1
+	if which stat > /dev/null 2>&1 && [ -f "$SWIFTFORMAT" ]; then
+		PRODUCT_MTIME=$(stat -c %Y "$SWIFTFORMAT")
+		PACKAGE_SWIFT_MTIME=$(stat -c %Y "./Package.swift")
+		PACKAGE_RESOLVED_MTIME=$(stat -c %Y "./Package.resolved")
+		if (( PRODUCT_MTIME > PACKAGE_SWIFT_MTIME && PRODUCT_MTIME > PACKAGE_RESOLVED_MTIME )); then
+			NEEDS_REBUILD=0
+		fi
+	fi
+	if (( NEEDS_REBUILD != 0 )); then
+		swift build --product swiftformat
+		touch "$SWIFTFORMAT"
+	fi
+	"$SWIFTFORMAT" --cache "$CACHE" .
 	# https://github.com/nicklockwood/SwiftFormat/issues/1904
-	./.build/debug/swiftformat --cache ./.build/swiftformat-cache.json --lint --lenient .
+	"$SWIFTFORMAT" --cache "$CACHE" --lint --lenient .
 elif which docker > /dev/null 2>&1; then
 	docker run --rm \
 		--volume .:/workspace \
