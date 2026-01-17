c0cef72f6caf5eff59ad03b99dbfda7636721530
Benedek Kozma <cyberbeni@gmail.com>
2026-01-17 12:14:12 +0100

    Update swift docker image in scripts too

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 pushd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null
 SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
 
-DOCKER_IMAGE="docker.io/swift:6.2.0"
+DOCKER_IMAGE="docker.io/swift:6.2.3"
 PROCESS="swift"
 
 do_it() {
