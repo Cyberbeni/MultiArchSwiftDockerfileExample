29a9b14a6af1382288c4185a4fc75d7b42b9a7a6
Benedek Kozma <cyberbeni@gmail.com>
2026-07-19 15:44:35 +0200

    Update Package.swift, also ask if want to keep Docker setup

--- template/bootstrap.sh
+++ template/bootstrap.sh
@@ -7,23 +7,40 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null
 echo "Choose project type:"
 echo "1 - Executable"
 echo "2 - Library"
-## TODO: remove other target from Package.swift
 while : ; do
 	read -r -p "> " -n 1 k <&1
 	if [[ $k = 1 ]] ; then
 		printf "\nSetting up executable project.\n"
 		rm -rf ./Sources/ExampleLib
 		rm ./.woodpecker/package-push.yaml
+		perl -0pe "s#\t\t\.library\(\n(.*\n)*?\t\t\),\n##g" -i ./Package.swift
+		perl -0pe "s#\t\t\.target\(\n(.*\n)*?\t\t\),\n##g" -i ./Package.swift
+		echo "Keep Docker setup?"
+		echo "1 - Yes"
+		echo "2 - No"
+		while : ; do
+			read -r -p "> " -n 1 k <&1
+			if [[ $k = 1 ]] ; then
+				echo ""
+				read -r -p $'Enter container image name (allowed characters: lowercase letters, digits, dashes):\n> ' CONTAINER_IMAGE_NAME
+				sed -i "s#multi-arch-swift-dockerfile-example#${CONTAINER_IMAGE_NAME}#" ./.woodpecker/docker-push.yaml
+				break
+			elif [[ $k = 2 ]] ; then
+				printf "\nRemoving Docker related files.\n"
+				rm ./Dockerfile ./.dockerignore ./.woodpecker/docker-push.yaml
+				break
+			fi
+		done
 		break
 	elif [[ $k = 2 ]] ; then
 		printf "\nSetting up library project.\n"
 		rm -rf ./Sources/ExampleApp
-		rm ./Dockerfile ./dockerignore ./scripts/assert-linked-libraries.sh ./scripts/build-release.sh ./.woodpecker/docker-push.yaml
+		rm ./Dockerfile ./.dockerignore ./scripts/assert-linked-libraries.sh ./scripts/build-release.sh ./.woodpecker/docker-push.yaml
+		perl -0pe "s#\t\t\.executable\(\n(.*\n)*?\t\t\),\n##g" -i ./Package.swift
+		perl -0pe "s#\t\t\.executableTarget\(\n(.*\n)*?\t\t\),\n##g" -i ./Package.swift
 		break
 	fi
 done
-
-exit 0
 
 ## Project name
 read -r -p $'Enter project name:\n> ' PROJECT_NAME
